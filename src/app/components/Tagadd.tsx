578a5e8f9e3e2fbd8b63fcfaa6314b7103225ff2
spell852 <vegaskevin46@gmail.com>
2024-09-06 22:25:33 -0400

    Added addTags component to add tags

--- src/app/components/Tagadd.tsx
+++ src/app/components/Tagadd.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+'use client'
+import React, {useState} from 'react';
 import IconTag from '../assets/Icons/Icontag';
-interface Props {
-  
-}
 
-const Props: React.FC<Props> = ({  }) => {
+
+const Tagsadd: React.FC = () => {
+
+    const [tags,setTags] = useState<string[]>([]); 
+    const [inputValue, setinputvalue] = useState<string>('');
+
+    //FUNCION PARA MANEJAR EL EVENTO CUANDO SE PRESIONA UNA TECLA (ENTER)
+    const handleKeyEnter = (event:React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter' && inputValue.trim()){
+            event.preventDefault();
+            setTags([...tags, inputValue.trim()]); // ESTO A;adira la etqieuta
+            setinputvalue(''); // Esto va a limpiar el input luego de a;adir la etiqueta
+        }
+    };
+
+    // FUNCION PARA ELIMINAR UNA ETIQUETA
+    const removeTag = (index:number) => {
+        setTags(tags.filter((_, i) => i  !== index));
+    }
+
   return (
     <>
         <div className='flex relative items-center'>
@@ -13,10 +30,30 @@ const Props: React.FC<Props> = ({  }) => {
         type='text'
         placeholder='add'
         className='px-10 py-2 rounded-2xl w-full outline-none bg-Paper'
+        value={inputValue}
+        onChange={(e) => setinputvalue(e.target.value)}
+        onKeyDown={handleKeyEnter}
         />
         </div> 
+
+        <div  className='flex flex-wrap gap-2'>
+                {tags.map((tag,index) =>(
+                    <div 
+                    key={index}
+                    className='rounded-2xl py-2 px-4 bg-Ocean'
+                    >
+                        <span className='font-medium text-Clouds'>{tag}</span>
+                        <button
+                        onClick={() => removeTag(index)}
+                        className='text-red-600 font-bold hover:text-yellow-950'
+                        >
+                            x
+                        </button>
+                    </div>
+                ))}
+        </div>
     </>
   );
 };
 
-export default Props;
+export default Tagsadd;
